af8b0f9278aca5165e10712faa7f81c7d4781687
Kei-Eff <karl.alberto@hotmail.com>
2021-09-17 10:14:26 +1000

    Created methods and initialisers

--- classes.py
+++ classes.py
@@ -1,14 +1,44 @@
+import random
+
 ##### Fruit Classes #####
 
 class Fruit():
-    pass
+    def __init__(self):
+        self.flavour, self.colour = random.choice(self.varieties)
+
+class Apple(Fruit):
+    varieties = [("sour", "green"), ("sweet", "red")]
+
+class Pear(Fruit):
+    varieties = [("mellow", "yellow"), ("sharp", "green")]
+
 
 ##### Tree Classes #####
 
 class  Tree():
-    pass
+    def __init__(self):
+        self.fruits = []
+
+class AppleTree(Tree):
+    fecundity = 8
+    fruit_type = Apple
+
+class PearTree(Tree):
+    fecundity = 5
+    fruit_type = Pear
+
+
 
 ##### Cider Classes #####
 
 class Cider():
-    pass
+    def __init__(self, fruitlist):
+        self.flavour = {
+            "sweet": 0,
+            "sour": 0,
+            "mellow": 0,
+            "sharp": 0
+        }
+
+        for fruit in fruitlist:
+            self.flavour[fruit.flavour] += 1
